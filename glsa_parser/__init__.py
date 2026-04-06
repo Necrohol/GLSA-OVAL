f0d4750fecd256fb06b0b72380cfa57bd5babf8c
Michael L. <mike@michaellawrenceit.com>
2026-04-06 10:39:22 -0500

    Update __init__.py

--- glsa_parser/__init__.py
+++ glsa_parser/__init__.py
@@ -1,1 +1,7 @@
+# glsa_parser/__init__.py
 
+# This allows: from glsa_parser import sync_glsa, generate_oval
+from .glsa_to_oval import sync_glsa, generate_oval
+
+__version__ = "2026.04.06"
+__author__ = "necrose99"
